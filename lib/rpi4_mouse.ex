33745d22d7ce92f36759f2d77e2be89949167597
Ryota Kinukawa <kinukawa@tombo-works.com>
2023-08-10 18:44:16 +0900

    Tweak rpi4_mouse.ex

--- lib/rpi4_mouse.ex
+++ lib/rpi4_mouse.ex
@@ -13,11 +13,21 @@ defmodule Rpi4Mouse do
   def init(_args) do
     if bin = System.find_executable("modprobe") do
       {_, 0} = System.cmd(bin, ~w"rtmouse.ko")
+      send(self(), :start)
     end
 
-    # Application.ensure_all_started(:raspimouse2_ex)
-    # |> tap(&Logger.info("#{__MODULE__}: ensure_all_started return is #{inspect(&1)}"))
-
     {:ok, %{}}
+  end
+
+  def handle_info(:start, state) do
+    # NOTE
+    # Nerves の Network 類の起動を待たないうちに起動してすると
+    # サブスクライバがなぜか動作しない（原因不明）ため決め打ちで待つ
+    Process.sleep(5_000)
+
+    Application.ensure_all_started(:raspimouse2_ex)
+    |> tap(&Logger.info("#{__MODULE__}: ensure_all_started return is #{inspect(&1)}"))
+
+    {:noreply, state}
   end
 end
