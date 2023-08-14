74c74f96d156c04ab39573479d5ae8bbbf76c9bc
Ryota Kinukawa <kinukawa@tombo-works.com>
2023-08-14 11:30:32 +0900

    Update raspimouse2_ex, can drive leds

--- config/config.exs
+++ config/config.exs
@@ -24,7 +24,8 @@ config :rclex,
   ros2_message_types: [
     "std_msgs/msg/Int16",
     "geometry_msgs/msg/Twist",
-    "raspimouse_msgs/msg/LightSensors"
+    "raspimouse_msgs/msg/LightSensors",
+    "raspimouse_msgs/msg/Leds"
   ]
 
 if Mix.target() == :host do
